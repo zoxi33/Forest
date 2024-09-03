25dc310893a03ca29f0ed544c8f786ebd22ddc00
Michał <michalgrzejdak9@gmail.com>
2024-09-03 20:39:37 +0200

    Full site with src

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon')
+const hamburger = document.querySelector('.hamburger')
+const closeButton = document.querySelector('.closeButton')
+const navMobile = document.querySelector('.menu-mobile')
+const allNavItems = document.querySelectorAll('.nav_link')
+const currentyear = document.querySelector('.currentyear')
+
+const showNav = () => {
+	navMobile.classList.toggle('active')
+	buttonSwitch()
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('displayingn')
+			
+			navMobile.classList.remove('active')
+		})
+	})
+}
+const buttonSwitch = () => {
+	hamburger.classList.toggle('displayingn')
+	
+}
+const hanldeCurrentYear = () => {
+	const year = new Date().getFullYear()
+	currentyear.innerText = year
+}
+hanldeCurrentYear();
+hamburger.addEventListener('click', showNav)
+closeButton.addEventListener('click', buttonSwitch)
+closeButton.addEventListener('click', () => {
+	navMobile.classList.remove('active')
+})
