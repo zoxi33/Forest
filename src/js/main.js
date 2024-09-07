df9a1d6961dea90972c47691e64b24e45e05db68
Michał <michalgrzejdak9@gmail.com>
2024-09-07 11:05:09 +0200

    naprawiony menu mobile z zdjeciami

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,8 @@ const closeButton = document.querySelector('.closeButton')
 const navMobile = document.querySelector('.menu-mobile')
 const allNavItems = document.querySelectorAll('.nav_link')
 const currentyear = document.querySelector('.currentyear')
-
 const showNav = () => {
 	navMobile.classList.toggle('active')
-	
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			navMobile.classList.remove('active')
@@ -15,26 +13,24 @@ const showNav = () => {
 	})
 }
 
-const hanldeCurrentYear = () => {
+const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	currentyear.innerText = year
 }
-hanldeCurrentYear();
-hamburger.addEventListener('click', showNav)
-closeButton.addEventListener('click', () => {
-	navMobile.classList.remove('active')
-})
-console.log(scrollY);
+handleCurrentYear()
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
-	
-	function addBackground(){
-		if(window.scrollY >=83){
+
+	function addBackground() {
+		if (window.scrollY >= 83) {
 			nav.classList.add('new-background')
-		}else{
+		} else {
 			nav.classList.remove('new-background')
-			
 		}
 	}
 	window.addEventListener('scroll', addBackground)
+})
+hamburger.addEventListener('click', showNav)
+closeButton.addEventListener('click', () => {
+	navMobile.classList.remove('active')
 })
